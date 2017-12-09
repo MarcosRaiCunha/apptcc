e28ed4ed5ae43ccfed5d627065d9a857f10bf6cd
Marcos Rai Alves da Cunha <marcosraicunha@gmail.com>
2017-12-09 18:19:21 -0200

    TCC OK

--- app.js
+++ app.js
@@ -82,5 +82,5 @@ app.use('/users', users);
 app.set('port', (process.env.port || 5000));
 
 app.listen(app.get('port'), function(){
-	console.log('Server started on port '+ app.get('port'));
+	console.log('Server started on port '+ app.get('port') + ' Acesse: localhost:5000');
 });
